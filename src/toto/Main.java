ddc5b88b8a46dbe0d1cf84706b89bb9035172033
Anis Ghedabnia <117355844+anis595@users.noreply.github.com>
2023-07-07 16:48:07 +0200

    add article class and bdd connection

--- src/toto/Main.java
+++ src/toto/Main.java
@@ -3,6 +3,10 @@ package toto;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+
 
 public class Main {
 
@@ -11,8 +15,23 @@ public class Main {
 		Connection conn = null;
 		try {
 		    conn =
-		       DriverManager.getConnection("jdbc:mysql://localhost:3306/reparation" +
+		       DriverManager.getConnection("jdbc:mysql://localhost/snackmounir?" +
 		                                   "user=root&password=");
+		    Statement stmt = conn.createStatement();
+		    String sql = "SELECT * FROM article";
+		    ResultSet res = stmt.executeQuery(sql);
+		    while(res.next()) {
+                int id = res.getInt("id");
+                String libelé = res.getString("libelé");
+                int prix = res.getInt("prix");
+                
+                System.out.printf("%d\t%s\t\t%d\n", 
+                        id,
+                        libelé,
+                        prix);
+                        
+        }
+		   
 
 		    // Do something with the Connection
 
