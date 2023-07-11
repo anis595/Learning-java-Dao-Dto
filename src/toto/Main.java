fb88ffb142adfaa29fa239747c7bc955a3752a9f
Anis Ghedabnia <117355844+anis595@users.noreply.github.com>
2023-07-11 16:08:37 +0200

    finish method for methode price

--- src/toto/Main.java
+++ src/toto/Main.java
@@ -7,10 +7,15 @@ public class Main {
 	public static void main(String[] args) {
 		ArticleDAOImp impl = new ArticleDAOImp();
 		Article salade = new Article(6, "salade", 5);
-		// impl.addArticle(salade);
-		// impl.deleteArticle(10);
+		impl.addArticle(salade);
+		impl.deleteArticle(10);
 		impl.editArticle( new Article (15, "saloon",6));
 		impl.getArticle(0);
 		impl.getAllArticle();
+		Service monService = new Service(new ArticleDAOImp()); 
+		
+		
+		monService.remiseArticle(impl.getAllArticle(), 2);
+		
 	}
 }
--- src/toto/Service.java
+++ src/toto/Service.java
@@ -1,5 +1,20 @@
 package toto;
 
+import java.util.List;
+
 public class Service {
+	
+	private ArticleDAOImp impl;
+	public Service(ArticleDAOImp impl) {
+		this.impl= impl;
+	}
+	 public void remiseArticle(List<Article> lesArticles , double remise) {
+		 for(Article unArticle : lesArticles) {
+			 unArticle.setPrix(unArticle.getPrix() * remise);
+			 impl.editArticle(unArticle);
+		 }
+		 
+		 
+	 }
 
 }
